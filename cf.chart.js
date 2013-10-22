b6604c6fedddbe5b7c66ffca3f933785b92aeb62
unknown <JDIXON@UKDTW786.cf.cheapflights.net>
2013-10-22 15:29:01 +0100

    Fixed up a bunch of things to do with the dynamic resizing of bars when adding new data

--- cf.chart.js
+++ cf.chart.js
@@ -1,3 +1,6 @@
+
+// TODO: Recalculate bar height when the bar that was the maximum is reduced to a lower number. A new maximum should be calculated based on current bars.
+
 (function($, ko, cf, undefined){
     if ($ === undefined) alert('jQuery not loaded.');
     if (ko === undefined) alert('KnockoutJS not loaded.');
@@ -20,8 +23,87 @@
     	self.metadata = {
     		maxValue: ko.observable(),
     		minValue: null,
-    		activeBars: ko.observable()
+    		activeBars: ko.observable(),
+    		xAxisValuesDisplayNumber: 4,
+    		yAxisValuesDisplayNumber: 4
     	};
+
+    	self.visibleXAxisValues = ko.observableArray();
+    	self.visibleYAxisValues = ko.observableArray();
+
+    	function setAxisViewProperties()
+    	{
+    		/*console.log("Setting up the visible X axis values");*/
+    		// grab all the bars that are curretly active.
+    		var allActive = $.grep(self.bars, function(bar){
+    			bar.displayXAxisValue(false);
+    			return bar.isActive();
+    		});
+
+    		
+    		if(allActive.length > 0)
+    		{
+				self.visibleYAxisValues.removeAll();
+
+		 		// Add the max value
+	    		self.visibleYAxisValues.push({
+	    			value: self.metadata.maxValue()
+	    		});
+
+				var yRef = self.metadata.maxValue() / self.metadata.yAxisValuesDisplayNumber;
+
+	    		// Add intermediate values
+	    		for(var i = self.metadata.yAxisValuesDisplayNumber -1; i > 1 ; i--)
+	    		{
+	    			self.visibleYAxisValues.push({
+	    				value: Math.ceil(yRef * i)
+	    			});
+	    		}
+
+	   			/// Add the min value
+	    		self.visibleYAxisValues.push(
+	    		{
+	    			value: 0
+	    		});
+
+	    		var ref = allActive.length / self.metadata.xAxisValuesDisplayNumber;
+
+	    		
+				self.visibleXAxisValues.removeAll();
+
+				allActive[0].displayXAxisValue(true);
+	    		self.visibleXAxisValues.push({
+	    			bar: allActive[0],
+	    			cssClass: 'xAxisFirst'
+	    		});
+
+	    		for(var i = 1; i < self.metadata.xAxisValuesDisplayNumber -1; i++)
+	    		{
+	    			var pos = Math.ceil(ref * i);
+	    			var item = allActive[pos];
+	    			if(item !== undefined)
+	    			{
+		    			item.displayXAxisValue(true);
+
+		    			self.visibleXAxisValues.push({
+		    				bar: item,
+		    				cssClass: ''
+		    			});
+	    			}
+	    		}
+
+	    		var lastItem = allActive[allActive.length - 1];
+	    		if(lastItem !== undefined)
+	    		{
+		    		lastItem.displayXAxisValue(true);
+
+		    		self.visibleXAxisValues.push({
+		    			bar: lastItem,
+		    			cssClass: 'xAxisLast'
+		    		});
+	    		}
+    		}
+    	}
 
     	function initialise()
     	{
@@ -38,26 +120,56 @@
 					});
 	    	});
 
+	    	var maxBarValueFound = 0;
 	    	// create a bar for each x horizontal axis value
 			$.each(self.xAxisValues, function(index, value){
 				var bar = {
 	    			xAxisValue: value,
 	    			yAxisValue: ko.observable(null),
 	    			yAxisValueAsPercent: ko.observable(null),
+	    			yAxisValueBeforeChange: null,
 	    			isActive: ko.observable(true),
 	    			xAxisValueAsPercent: ko.computed(function(){
-	    				return 90 / self.metadata.activeBars();
-	    			})
+	    				return 100 / self.metadata.activeBars();
+	    			}),
+	    			displayXAxisValue: ko.observable(false)
 	    		};
 
 	    		bar.yAxisValue.subscribe(function(value){
+
 	    			if(value > self.metadata.maxValue())
 	    			{
 	    				self.metadata.maxValue(value);
+	    			} 
+	    			else if(bar.yAxisValueBeforeChange == self.metadata.maxValue()) 
+	    			{
+	    				// if this value is the same as the max value we need to figure out what the max value bar is
+	    				// and set that value as the max value.
+
+	    				console.log("This bar used to be at the max. Now we need to decide if the max should change!");
+
+	    				var valuesArray = $.map(self.bars, function(item){ return item.yAxisValue(); });
+	    				var maxBarValue = Math.max.apply(Math, valuesArray);
+
+	    				console.log("the max bar value we have is :" + maxBarValue);
+	    				// If the max bar we have is less than the max value of the chart
+	    				// reset the max value of the chart to be the max bar value.
+	    				if(maxBarValue < self.metadata.maxValue())
+	    				{
+	    					console.log("The max value is now too big. Let's reduce this shizzle.");
+	    					self.metadata.maxValue(maxBarValue)
+	    				}
 	    			}
+
 	    			/*self.metadata.maxValue(value > self.metadata.maxValue() ? value : self.metadata.maxValue());*/
 	    			bar.yAxisValueAsPercent((value / self.metadata.maxValue()) * 100);
 	    		});
+
+	    		bar.yAxisValue.subscribe(function(value){
+	    			
+					bar.yAxisValueBeforeChange = value;
+
+	    		}, null, "beforeChange");
 
 				self.bars.push(bar);
 			});
@@ -71,27 +183,29 @@
     	self.trimBars = function()
     	{
     		function testBarForRemove(isSeenValue, bar)
-					{
-						// If we're yet to see a populated bar and the current bar's value is null
-						// remove the bar. Otherwise we've seen a populated bar and we return true.
-						(!isSeenValue && bar.yAxisValue() === null) 
-							? function(){ bar.isActive(false);  self.removedBars += 1; self.metadata.activeBars(self.metadata.activeBars()-1); }() 
-							: isSeenValue = true;
+			{
+				// If we're yet to see a populated bar and the current bar's value is null
+				// remove the bar. Otherwise we've seen a populated bar and we return true.
+				(!isSeenValue && bar.yAxisValue() === null) 
+					? function(){ 
+						bar.isActive(false);  
+						self.removedBars += 1; 
+						self.metadata.activeBars(self.metadata.activeBars()-1); 
+					}() 
+					: isSeenValue = true;
 
-							if(isSeenValue)
-							{
-								console.log(bar);
-							}
-
-						return isSeenValue;
-					}
+				return isSeenValue;
+			}
 
 			var seenValueLeft, seenValueRight;
 			var bars = self.bars;
 			var barLast = bars.length -1;
 
 			var currentBar = 0;
-			while((!seenValueLeft || !seenValueRight)) 
+
+			// Loop through the bars both left and right and hide all zero value bars until we reach a bar with a value.
+			// Hiding means we set the isActive property of the bar to false. We never remove bars from the graph.
+			while((!seenValueLeft || !seenValueRight) && currentBar <= bars.length / 2) 
 			{
 				// remove first and last bars is appropriate (if their value is 0)
 				seenValueLeft = !seenValueLeft 
@@ -105,9 +219,8 @@
 				barLast -= 1;
 			}
 
-			// Resize bars
-			var bars = self.getBars();
-			$.each(bars, function(index, value){ value.xAxisValueAsPercent(); });
+			setAxisViewProperties();
+
 			return self.getBars();
     	}
 
@@ -133,18 +246,16 @@
 
     		// Find all the bars that match our xAxisReference.
     		var matchingBars = $.grep(bars, function(item, index) {
-
 				// if we have an exact match on the x axis, we know we can update this bar.
-				if(xAxisReference == item.xAxisValue)
+				if(xAxisReference === item.xAxisValue)
 				{
 					return true;
 				}
-
-				// if the xAxisReference falls between two bars, we can update the bar at the lower boundary.
-				// TODO: Check this assumption. Do we even want to handle this case?
-				if(index < bars.length)
-				{
-					return xAxisReference > item.xAxisValue && xAxisReference < bars[index + 1].xAxisValue;
+				else if(index < (bars.length)){
+						// TODO: This comparison might be working ok for my numeric values but it doesn't work well for alpha strings.
+						// Need a better way of specifying comparison provider.
+						var isIndirectMatch = xAxisReference > item.xAxisValue && xAxisReference < bars[index + 1].xAxisValue;
+						return isIndirectMatch;
 				}
 
 				// We couldn't find any bars that match our criteria
@@ -155,6 +266,7 @@
     		if(matchingBars.length > 0)
     		{
     			// If the bar we're adding is about to become active for the first time, incriment the activeBars count.
+    			// TODO: Move this logic to a subscription on isActive and test against the old value vs new value.
     			if(!matchingBars[0].isActive())
     			{
     				self.metadata.activeBars(self.metadata.activeBars() + 1);
@@ -163,6 +275,8 @@
     			matchingBars[0].yAxisValue(newYAxisValue);
     			matchingBars[0].isActive(true);
     			
+    			// Check if there are any non-active bars between the bars with values.
+    			// All bars between active bars should also be active.
     			var x = 0;
     			var y = bars.length -1;
     			var firstActiveIndex = null;
@@ -171,6 +285,7 @@
     			var firstActiveFound = false;
     			var lastActiveFound = false;
 
+    			// Find the first active bars on the left and right of the chart.
     			while(!firstActiveFound || !lastActiveFound)
     			{
     				if(!firstActiveFound && bars[x].isActive())
@@ -189,7 +304,7 @@
     				y-=1;
     			}
 
-    			// Set all applicable bars to active.
+    			// For all bars between the first active bars on the left and right, set them to active.
     			for(var i = firstActiveIndex; i < lastActiveIndex; i++)
     			{
     				if(!bars[i].isActive())
@@ -201,13 +316,17 @@
     				bars[i].yAxisValue(bars[i].yAxisValue());
     			}
     		}
+
+    		setAxisViewProperties();
     	};
 
     	self.getBars = function(){
+    		// Set the min and max values in the chart.
     		var valuesArray = $.map(self.bars, function(item){ return item.yAxisValue(); });
 			self.metadata.maxValue(Math.max.apply(Math, valuesArray));
 			self.metadata.minValue = Math.min.apply(Math, valuesArray);
 
+			// TODO: This could be a calculated observable?
 			$.each(self.bars, function(index, value){
 				value.yAxisValueAsPercent((value.yAxisValue() / self.metadata.maxValue()) * 100);
 			});
@@ -271,9 +390,9 @@
                     {
                     	return;
                     }
-					var bar = $("<div/>", { class: 'bar'});
+					var bar = $("<div/>", { 'class': 'bar'});
 					bar.data('value', value.yAxisValue());
-					var barColour = $("<div/>", { class: 'barColour', css: { height: value.yAxisValueAsPercent().toString() + "%" }});
+					var barColour = $("<div/>", { 'class': 'barColour', css: { height: value.yAxisValueAsPercent().toString() + "%" }});
 					var barValue = $("<span/>", { text: value.xAxisValue	});					
 					self.chartContainer.append(bar.append(barColour.append(barValue)));
 					self.activeBars += 1;
@@ -282,9 +401,12 @@
 				self.getChart = function(){
 					function testBarForRemove(isSeenValue, bar)
 					{
+						console.log("testing bar:");
+						console.log(bar);
+						console.log(bar.data("value"));
 						// If we're yet to see a populated bar and the current bar's value is null
 						// remove the bar. Otherwise we've seen a populated bar and we return true.
-						(!isSeenValue && bar.data("value") === undefined) 
+						(!isSeenValue && bar.data("value") === null) 
 							? function(){ bar.removeClass('activeBar'); self.removedBars += 1 }() 
 							: isSeenValue = true;	
 						return isSeenValue;
@@ -300,12 +422,15 @@
 						resizeBarsAsPercentage(100 / activeBars);
 					}
 
+					
+					var bars = $(".bar", self.chartContainer);
+					bars.addClass("activeBar");
+
 					if(self.settings.trimBars)
 					{
 						var seenValueLeft, seenValueRight;
 						var bars = $(".bar", self.chartContainer);
 						var barLast = bars.length -1;
-						$(".bar", self.chartContainer).addClass("activeBar");
 
 						var x = 0;
 						while((!seenValueLeft || !seenValueRight) && x < 20) 
@@ -322,10 +447,12 @@
 
 													x +=1;
 						}
-
-						// Resize bars
 						resizeBarsAsPercentage(100 / (bars.length - self.removedBars));
 					}
+					else{
+						resizeBarsAsPercentage(100 / bars.length);
+					}
+					
 
 					return self.chartContainer;
 				};
